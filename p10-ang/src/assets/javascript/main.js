9ebadd0b6ba1dba43c7ca04a6f884378822bbc8b
damien diep <damien.diep@gmail.com>
2018-06-08 11:27:14 +0200

    Modif img parallax et navbar sur la page Accueil

--- p10-ang/src/assets/javascript/main.js
+++ p10-ang/src/assets/javascript/main.js
@@ -2,3 +2,30 @@ document.addEventListener('DOMContentLoaded', function () {
     M.AutoInit();
     $('.sidenav').sidenav();
 });
+$(document).ready(function () {
+    //Effet parallax sur la page d'accueil
+    let imgPos1 = document.querySelector("#img-parallax");
+    imgPos1 = imgPos1.clientTop;
+    let imgPos;
+    $(window).scroll(function () {
+        imgPos = $(this).scrollTop();
+        $("#img-parallax").css({
+            'top': imgPos1 + imgPos / 2 + 'px'
+        });
+    });
+
+    // Montre/cache la navbar sur un scroll
+    $(window).bind('mousewheel', function(event) {
+        if (event.originalEvent.wheelDelta >= 0) {
+            $("nav").css({
+                'top': '0px'
+            });
+        }
+        else {
+            $("nav").css({
+                'top': '-80px'
+            });
+        }
+    });
+
+});
